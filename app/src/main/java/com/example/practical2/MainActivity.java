bc62ded369031d28e25ee275906e9d2c3a53a890
Trey Murray <77852326+TreyMurray00@users.noreply.github.com>
2022-10-06 10:43:38 -0400

    instantiation

--- app/src/main/java/com/example/practical2/MainActivity.java
+++ app/src/main/java/com/example/practical2/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.practical2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private ImageButton imageButton;
+    private ImageView imageView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner  = (Spinner) findViewById(R.id.spinner2);
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        editText = (EditText) findViewById(R.id.editTextTextMultiLine);
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
 }
